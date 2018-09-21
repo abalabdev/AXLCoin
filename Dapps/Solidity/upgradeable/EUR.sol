8ceaccb53f3a4024a5cce90d91a73c3d2b3100db
hrrrrr <40416920+hrrrrr@users.noreply.github.com>
2018-09-21 14:26:31 +0900

    delete
    delet

--- Dapps/Solidity/upgradeable/EUR.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.11;
-
-import "./TokenFrontend.sol";
-
-contract EUR is TokenFrontend {
-    function EUR(address _controller) 
-        TokenFrontend("Smart EUR", "EURS", "EUR", _controller) 
-    { }
-}
--- Dapps/Solidity/upgradeable/USD.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.11;
-
-import "./TokenFrontend.sol";
-
-contract USD is TokenFrontend {
-    function USD(address _controller) 
-        TokenFrontend("Smart USD", "USDS", "USD", _controller) 
-    { }
-}
